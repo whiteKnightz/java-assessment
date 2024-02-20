a4ef9a957e0346b8f0d0f40d1dc6baa7b9ed886b
Abhishek Satyal <ab.satyal@gmail.com>
2024-02-20 11:43:41 -0500

    feat: solved Delete Node In A graphs - BFS

--- src/com/whiteknightz/demo/leetCode/graphsBFS/NearestExitFromEntranceInMaze.java
+++ src/com/whiteknightz/demo/leetCode/graphsBFS/NearestExitFromEntranceInMaze.java
@@ -1,2 +1,101 @@
-package com.whiteknightz.demo.leetCode.graphsBFS;public class NearestExitFromEntranceInMaze {
+package com.whiteknightz.demo.leetCode.graphsBFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+    You are given an m x n matrix maze (0-indexed) with empty cells (represented as '.') and walls (represented as '+').
+    You are also given the entrance of the maze, where entrance = [entrancerow, entrancecol] denotes the row and column
+    of the cell you are initially standing at.
+
+    In one step, you can move one cell up, down, left, or right. You cannot step into a cell with a wall, and you cannot
+    step outside the maze. Your goal is to find the nearest exit from the entrance. An exit is defined as an empty cell
+    that is at the border of the maze. The entrance does not count as an exit.
+
+    Return the number of steps in the shortest path from the entrance to the nearest exit, or -1 if no such path exists.
+
+
+
+    Example 1:
+
+
+    Input: maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], entrance = [1,2]
+    Output: 1
+    Explanation: There are 3 exits in this maze at [1,0], [0,2], and [2,3].
+    Initially, you are at the entrance cell [1,2].
+    - You can reach [1,0] by moving 2 steps left.
+    - You can reach [0,2] by moving 1 step up.
+    It is impossible to reach [2,3] from the entrance.
+    Thus, the nearest exit is [0,2], which is 1 step away.
+    Example 2:
+
+
+    Input: maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]
+    Output: 2
+    Explanation: There is 1 exit in this maze at [1,2].
+    [1,0] does not count as an exit since it is the entrance cell.
+    Initially, you are at the entrance cell [1,0].
+    - You can reach [1,2] by moving 2 steps right.
+    Thus, the nearest exit is [1,2], which is 2 steps away.
+    Example 3:
+
+
+    Input: maze = [[".","+"]], entrance = [0,0]
+    Output: -1
+    Explanation: There are no exits in this maze.
+
+ */
+public class NearestExitFromEntranceInMaze {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int m = maze.length;
+        int n = maze[0].length;
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[]{entrance[0], entrance[1], 0});
+        maze[entrance[0]][entrance[1]] = '+';
+
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int step = poll[2];
+
+            if ((x != entrance[0] || y != entrance[1]) && (x == 0 || x == m - 1 || y == 0 || y == n - 1)) {
+                return step;
+            }
+            for (int[] dir : dirs) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && maze[newX][newY] == '.') {
+                    maze[newX][newY] = '+';
+                    queue.offer(new int[]{newX, newY, step + 1});
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        NearestExitFromEntranceInMaze maze = new NearestExitFromEntranceInMaze();
+
+        char[][] mazeVal1 = {{'+', '+', '.', '+'}, {'.', '.', '.', '+'}, {'+', '+', '+', '.'}};
+        int[] entrance1 = {1, 2};
+        System.out.println("The expected value is 1 and is: " + maze.nearestExit(mazeVal1, entrance1));
+
+        char[][] mazeVal2 = {{'+', '+', '+'}, {'.', '.', '.'}, {'+', '+', '+'}};
+        int[] entrance2 = {1, 0};
+        System.out.println("The expected value is 2 and is: " + maze.nearestExit(mazeVal2, entrance2));
+
+        char[][] mazeVal3 = {{'.', '+'}};
+        int[] entrance3 = {0, 0};
+        System.out.println("The expected value is -1 and is: " + maze.nearestExit(mazeVal3, entrance3));
+
+        char[][] mazeVal = {{'.'}, {'.'}, {'.'}, {'.'}};
+        int[] entrance = {2, 0};
+        System.out.println("The expected value is 1 and is: " + maze.nearestExit(mazeVal, entrance));
+
+    }
 }
--- src/com/whiteknightz/demo/leetCode/graphsBFS/RottingOranges.java
+++ src/com/whiteknightz/demo/leetCode/graphsBFS/RottingOranges.java
@@ -1,4 +1,141 @@
 package com.whiteknightz.demo.leetCode.graphsBFS;
 
-public class RottinOranges {
+/*
+    You are given an m x n grid where each cell can have one of three values:
+
+    0 representing an empty cell,
+    1 representing a fresh orange, or
+    2 representing a rotten orange.
+    Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+    Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible,
+    return -1.
+
+
+
+    Example 1:
+
+
+    Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+    Output: 4
+    Example 2:
+
+    Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+    Output: -1
+    Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens
+    4-directionally.
+    Example 3:
+
+    Input: grid = [[0,2]]
+    Output: 0
+    Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RottingOranges {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int freshOranges = 0;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for(int i = 0;  i < m; i++){
+            for(int j = 0;  j < n; j++){
+                if (grid[i][j] == 2){
+                    queue.offer(new int[]{i, j});
+                } else if(grid[i][j] == 1) {
+                    freshOranges++;
+                }
+            }
+        }
+
+        int minutes = 0;
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty() && freshOranges > 0){
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] poll = queue.poll();
+                int x = poll[0];
+                int y = poll[1];
+
+                for (int[] dir : dirs) {
+                    int newX = x + dir[0];
+                    int newY = y + dir[1];
+
+                    if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 1){
+                        grid[newX][newY] = 2;
+                        queue.offer(new int[]{newX, newY});
+                        freshOranges--;
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return freshOranges == 0 ? minutes : -1;
+    }
+
+    public int orangesRotting1(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int freshOranges = 0;
+
+        // Initialize the queue with rotten oranges and count fresh oranges
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    freshOranges++;
+                }
+            }
+        }
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int minutes = 0;
+
+        while (!queue.isEmpty() && freshOranges > 0) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] poll = queue.poll();
+                int x = poll[0];
+                int y = poll[1];
+
+                for (int[] dir : directions) {
+                    int newX = x + dir[0];
+                    int newY = y + dir[1];
+
+                    if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 1) {
+                        grid[newX][newY] = 2;
+                        queue.offer(new int[]{newX, newY});
+                        freshOranges--;
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return freshOranges == 0 ? minutes : -1;
+    }
+
+    public static void main(String[] args) {
+        RottingOranges rottingOranges = new RottingOranges();
+
+        int[][] grid1 = {{2,1,1},{1,1,0},{0,1,1}};
+        System.out.println("The expected value is 4 and is: " + rottingOranges.orangesRotting(grid1));
+
+        int[][] grid2 = {{2,1,1},{0,1,1},{1,0,1}};
+        System.out.println("The expected value is -1 and is: " + rottingOranges.orangesRotting(grid2));
+
+        int[][] grid = {{0,2}};
+        System.out.println("The expected value is 0 and is: " + rottingOranges.orangesRotting(grid));
+
+    }
 }
